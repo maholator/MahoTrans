7328dba27750c6546810febd8b91b76adbc4319c
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-09-30 15:42:26 +0300

    Add constructors for some exceptions

--- MahoTrans/java/lang/IllegalArgumentException.cs
+++ MahoTrans/java/lang/IllegalArgumentException.cs
@@ -1,6 +1,11 @@
+using MahoTrans.Native;
+
 namespace java.lang;
 
 public class IllegalArgumentException : RuntimeException
 {
-    
+    [InitMethod]
+    public void Init()
+    {
+    }
 }
--- MahoTrans/java/lang/RuntimeException.cs
+++ MahoTrans/java/lang/RuntimeException.cs
@@ -1,6 +1,12 @@
+using MahoTrans.Native;
+using MahoTrans.Runtime;
+
 namespace java.lang;
 
 public class RuntimeException : Exception
 {
-    
+    [InitMethod]
+    public void Init([String] Reference msg)
+    {
+    }
 }
