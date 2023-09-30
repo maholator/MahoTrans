014fe5f0e1321e9e27805652258e63171225b250
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-09-30 22:54:03 +0300

    Add more methods for vector

--- MahoTrans/java/util/Vector.cs
+++ MahoTrans/java/util/Vector.cs
@@ -26,15 +26,11 @@ public class Vector : Object
         List = new List<Reference>(capacity);
     }
 
-    public Reference elementAt(int i) => List[i];
-
     public void addElement(Reference r) => List.Add(r);
 
     public int capacity() => List.Capacity;
 
-    public int size() => List.Count;
-
-    public bool isEmpty() => List.Count == 0;
+    public bool contains(Reference r) => List.Contains(r);
 
     public void copyInto([JavaType("[Ljava/lang/Object;")] Reference arr)
     {
@@ -42,11 +38,7 @@ public class Vector : Object
         List.CopyTo(a);
     }
 
-    public void removeAllElements() => List.Clear();
-
-    public void trimToSize()
-    {
-    }
+    public Reference elementAt(int i) => List[i];
 
     [return: JavaType(typeof(Enumeration))]
     public Reference elements()
@@ -54,5 +46,28 @@ public class Vector : Object
         var e = Heap.AllocateObject<ArrayEnumerator>();
         e.Value = List.ToArray();
         return e.This;
+    }
+
+    public void ensureCapacity(int min)
+    {
+    }
+
+    public Reference firstElement()
+    {
+        if (List.Count == 0)
+            Heap.Throw<NoSuchElementException>();
+        return List[0];
+    }
+
+    public int size() => List.Count;
+
+    public bool isEmpty() => List.Count == 0;
+
+    public bool removeElement(Reference obj) => List.Remove(obj);
+
+    public void removeAllElements() => List.Clear();
+
+    public void trimToSize()
+    {
     }
 }
