e008396e56bfb47d7ee9d5624cb8eb26f89dc55e
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-10-06 23:54:56 +0300

    Add `EOFException`

---
+++ MahoTrans/java/io/EOFException.cs
@@ -0,0 +1,17 @@
+using MahoTrans.Native;
+using MahoTrans.Runtime;
+
+namespace java.io;
+
+public class EOFException : IOException
+{
+    [InitMethod]
+    public void Init([String] Reference msg)
+    {
+    }
+
+    [InitMethod]
+    public void Init()
+    {
+    }
+}
