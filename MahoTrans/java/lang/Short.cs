d4dbe2dde02ffcf0c97f81a65f40fb4e6312b294
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-10-13 00:42:34 +0300

    Add get/set/equals for `Short`

--- MahoTrans/java/lang/Short.cs
+++ MahoTrans/java/lang/Short.cs
@@ -1,6 +1,32 @@
+using MahoTrans.Native;
+using MahoTrans.Runtime;
+
 namespace java.lang;
 
 public class Short : Object
 {
-    
+    [JavaIgnore] public short Value;
+
+    [InitMethod]
+    public void Init(short v)
+    {
+        Value = v;
+    }
+
+    public short shortValue() => Value;
+
+    public bool equals(Reference r)
+    {
+        if (r.IsNull)
+            return false;
+
+        var obj = Heap.ResolveObject(r);
+
+        if (obj is Short s)
+        {
+            return s.Value == Value;
+        }
+
+        return false;
+    }
 }
