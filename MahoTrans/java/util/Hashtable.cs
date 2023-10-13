03a6e2e651d96eb367a24f1b65908ce00e2bd414
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-10-13 13:56:12 +0300

    Make hashtable actually compare objects

--- MahoTrans/java/util/Hashtable.cs
+++ MahoTrans/java/util/Hashtable.cs
@@ -1,6 +1,8 @@
 using java.lang;
+using MahoTrans;
 using MahoTrans.Native;
 using MahoTrans.Runtime;
+using MahoTrans.Runtime.Types;
 using Object = java.lang.Object;
 
 namespace java.util;
@@ -23,8 +25,52 @@ public class Hashtable : Object
 
     public Reference get(Reference key)
     {
+        if (key.IsNull)
+            return Reference.Null;
         if (_storage.TryGetValue(key, out var value))
             return value;
+
+        //TODO this is unsafe and slow!
+
+        var pointer = Heap.State.GetVirtualPointer(new NameDescriptor("equals", "(Ljava/lang/Object;)Z"));
+        var method = Heap.State.GetVirtualMethod(pointer, key);
+        if (method.Class.IsObject)
+        {
+            // default equality comparer checks reference equality. We already did that.
+            return Reference.Null;
+        }
+
+        if (method.IsNative)
+        {
+            var res = Heap.ResolveObject(key);
+            var func = method.NativeBody.CreateDelegate<Func<Reference, bool>>(res);
+            foreach (var kvp in _storage)
+            {
+                if (func.Invoke(kvp.Key))
+                    return kvp.Value;
+            }
+
+            return Reference.Null;
+        }
+
+        var retFrame = new Frame(new JavaMethodBody(1, 0)
+        {
+            RawCode = new[] { new Instruction(JavaOpcode.@return) },
+            Method = new Method(new NameDescriptor("", ""), MethodFlags.Static, new JavaClass())
+        });
+        var equalityChecker = new JavaThread(retFrame, Heap.State);
+        foreach (var kvp in _storage)
+        {
+            equalityChecker.ActiveFrameIndex = 0;
+            var f = equalityChecker.Push(method.JavaBody);
+            f.PushReference(key);
+            f.PushReference(kvp.Key);
+            equalityChecker.Execute(Heap.State);
+            bool equal = equalityChecker.CallStack[0]!.Stack[0] != 0;
+            if (equal)
+                return kvp.Value;
+        }
+
         return Reference.Null;
     }
 
@@ -32,6 +78,7 @@ public class Hashtable : Object
     {
         if (key.IsNull || val.IsNull)
             Heap.Throw<NullPointerException>();
+
         if (_storage.TryGetValue(key, out var prev))
         {
             _storage[key] = val;
