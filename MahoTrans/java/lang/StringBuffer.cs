e5d8b460a0a9f4c521e2c2457641ae89d3c49ecf
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-10-14 01:41:27 +0300

    Add `append` for SB with object

--- MahoTrans/java/lang/StringBuffer.cs
+++ MahoTrans/java/lang/StringBuffer.cs
@@ -1,5 +1,8 @@
+using MahoTrans;
 using MahoTrans.Native;
 using MahoTrans.Runtime;
+using MahoTrans.Runtime.Types;
+using MahoTrans.Utils;
 
 namespace java.lang;
 
@@ -66,6 +69,27 @@ public class StringBuffer : Object
     {
         _buffer.AddRange(l.ToString());
         return This;
+    }
+
+    [JavaDescriptor("(Ljava/lang/Object;)Ljava/lang/StringBuffer;")]
+    public JavaMethodBody append(JavaClass cls)
+    {
+        // this, arg
+        return new JavaMethodBody(3, 2)
+        {
+            RawCode = new Instruction[]
+            {
+                new(JavaOpcode.aload_0),
+                new(JavaOpcode.dup),
+                new(JavaOpcode.aload_1),
+                new(JavaOpcode.invokevirtual,
+                    cls.PushConstant(new NameDescriptor("toString", "()Ljava/lang/String;")).Split()),
+                new(JavaOpcode.invokevirtual,
+                    cls.PushConstant(new NameDescriptor("append", "(Ljava/lang/String;)Ljava/lang/StringBuffer;"))
+                        .Split()),
+                new(JavaOpcode.areturn),
+            }
+        };
     }
 
     public int length() => _buffer.Count;
