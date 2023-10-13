0603d066f31c52f4dd78be10f9c0705d7ce92862
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-10-13 17:31:00 +0300

    Add constructor for IOExc

--- MahoTrans/java/io/IOException.cs
+++ MahoTrans/java/io/IOException.cs
@@ -1,8 +1,12 @@
+using MahoTrans.Native;
 using Exception = java.lang.Exception;
 
 namespace java.io;
 
 public class IOException : Exception
 {
-    
+    [InitMethod]
+    public void Init()
+    {
+    }
 }
