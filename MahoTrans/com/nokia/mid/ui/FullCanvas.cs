bd4fa7edf2513095a097daf89d166743340636ab
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-10-13 17:30:34 +0300

    Add stub for `FullCanvas`

---
+++ MahoTrans/com/nokia/mid/ui/FullCanvas.cs
@@ -0,0 +1,13 @@
+using javax.microedition.lcdui;
+using MahoTrans.Native;
+
+namespace com.nokia.mid.ui;
+
+public class FullCanvas : Canvas
+{
+    [InitMethod]
+    public override void Init()
+    {
+        base.Init();
+    }
+}
