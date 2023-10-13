b3f97bbc663892b9e8f9ee6414eca87ccde0bdd4
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-10-13 14:53:11 +0300

    Add runtime class

--- MahoTrans/java/lang/Runtime.cs
+++ MahoTrans/java/lang/Runtime.cs
@@ -1,6 +1,26 @@
+using MahoTrans.Native;
+using MahoTrans.Runtime;
+
 namespace java.lang;
 
-public class Runtime
+public class Runtime : Object
 {
-    
+    public static Reference Instance;
+
+    [return: JavaType(typeof(Runtime))]
+    public static Reference getRuntime()
+    {
+        if (Instance.IsNull)
+        {
+            Instance = Heap.AllocateObject<Runtime>().This;
+        }
+
+        return Instance;
+    }
+
+    public void gc() => System.gc();
+
+    public long freeMemory() => 1024 * 1024 * 3;
+
+    public long totalMemory() => 1024 * 1024 * 4;
 }
