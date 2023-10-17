6b4fc32585265ea1373b443074e10810e17f60af
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-10-18 01:22:25 +0300

    Implement `Date`

--- MahoTrans/java/util/Date.cs
+++ MahoTrans/java/util/Date.cs
@@ -1,7 +1,19 @@
+using MahoTrans.Native;
 using Object = java.lang.Object;
 
 namespace java.util;
 
 public class Date : Object
 {
+    private long _time;
+
+    [InitMethod]
+    public void Init(long ms)
+    {
+        _time = ms;
+    }
+
+    public long getTime() => _time;
+
+    public void setTime(long time) => _time = time;
 }
