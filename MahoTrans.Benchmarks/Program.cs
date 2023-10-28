129aa6013fe85c2d113d878ff9e3e51cf1f71019
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-10-29 01:51:32 +0300

    Drop unused benchmark

--- MahoTrans.Benchmarks/Program.cs
+++ MahoTrans.Benchmarks/Program.cs
@@ -1,8 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using BenchmarkDotNet.Running;
-using MahoTrans.Benchmarks;
-
 int i = 0;
 
-BenchmarkRunner.Run<DictsBenchs>();
+//BenchmarkRunner.Run<DictsBenchs>();
