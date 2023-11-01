cc2203c576650f64c86ac87b197e9e2dd6f0e95c
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-11-02 00:12:04 +0300

    Add missing members for throwable

--- MahoTrans/java/lang/Throwable.cs
+++ MahoTrans/java/lang/Throwable.cs
@@ -5,12 +5,31 @@ namespace java.lang;
 
 public class Throwable : Object
 {
+    [String] public Reference Message;
+
+    [InitMethod]
+    public void Init()
+    {
+        Message = Reference.Null;
+    }
+
     [InitMethod]
     public void Init([String] Reference message)
     {
+        Message = message;
     }
+
     public void printStackTrace()
     {
         Jvm.Toolkit.System.PrintException(this);
+    }
+
+    [return: String]
+    public Reference getMessage() => Message;
+
+    [return: String]
+    public Reference toString()
+    {
+        return Jvm.AllocateString($"{JavaClass.Name.Replace('/', '.')}: {Jvm.ResolveString(Message)}");
     }
 }
