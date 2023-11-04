b4cdbfa9ab0426e6b17c9db9b739d7a0a4753df5
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-11-04 03:41:28 +0300

    Bring `Byte` in line with CLDC specs

--- MahoTrans/java/lang/Byte.cs
+++ MahoTrans/java/lang/Byte.cs
@@ -1,4 +1,5 @@
 using MahoTrans.Native;
+using MahoTrans.Runtime;
 
 namespace java.lang;
 
@@ -10,4 +11,50 @@ public class Byte : Object
     public void Init(sbyte b) => Value = b;
 
     public sbyte byteValue() => Value;
+
+    public static int parseByte([String] Reference str)
+    {
+        if (!int.TryParse(Jvm.ResolveString(str), out var i))
+            Jvm.Throw<NumberFormatException>();
+
+        if (i < -128 || i > 127)
+            Jvm.Throw<NumberFormatException>();
+
+        return (sbyte)i;
+    }
+
+    public static int parseByte([String] Reference str, int radix)
+    {
+        try
+        {
+            var i = Convert.ToInt32(Jvm.ResolveString(str), radix);
+            if (i < -128 || i > 127)
+                Jvm.Throw<NumberFormatException>();
+            return (sbyte)i;
+        }
+        catch
+        {
+            Jvm.Throw<NumberFormatException>();
+        }
+
+        return 0;
+    }
+
+    [return: String]
+    public Reference toString()
+    {
+        return Jvm.AllocateString(Value.ToString());
+    }
+
+    public int hashCode() => Value;
+
+    public bool equals(Reference obj)
+    {
+        if (obj.IsNull)
+            return false;
+        var o = Jvm.ResolveObject(obj);
+        if (o is not Byte ii)
+            return false;
+        return ii.Value == Value;
+    }
 }
