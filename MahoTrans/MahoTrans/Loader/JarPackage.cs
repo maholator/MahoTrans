f6e0211be625afbb705241176e6c28b21125f3c7
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-11-05 02:59:58 +0300

    Add utils for midlets info retrieval

--- MahoTrans/MahoTrans/Loader/JarPackage.cs
+++ MahoTrans/MahoTrans/Loader/JarPackage.cs
@@ -17,4 +17,44 @@ public class JarPackage
         Resources = resources;
         Manifest = manifest;
     }
+
+    private static string GetMidletKey(int i) => $"MIDlet-{i}";
+
+    public int GetMidletsCount()
+    {
+        for (int i = 1; true; i++)
+        {
+            var key = GetMidletKey(i);
+
+            if (!Manifest.ContainsKey(key))
+            {
+                return i - 1;
+            }
+        }
+    }
+
+    public string? GetMidletName(int n)
+    {
+        return GetMidletInfoLine(n)?[0].Trim();
+    }
+
+    public string? GetMidletIcon(int n)
+    {
+        var icon = GetMidletInfoLine(n)?[1].Trim();
+        if (string.IsNullOrEmpty(icon))
+            return null;
+        return icon;
+    }
+
+    public string? GetMidletClass(int n)
+    {
+        return GetMidletInfoLine(n)?[2].Trim();
+    }
+
+    private string[]? GetMidletInfoLine(int n)
+    {
+        if (Manifest.TryGetValue(GetMidletKey(n), out var l))
+            return l.Split(',', 3, StringSplitOptions.TrimEntries);
+        return null;
+    }
 }
