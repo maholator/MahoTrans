809b7f2e6381df3b79d46bf2ac1779d2f7303a50
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-11-04 03:35:59 +0300

    Bring `lang.Math` in line with CLDC specs

--- MahoTrans/java/lang/Math.cs
+++ MahoTrans/java/lang/Math.cs
@@ -6,24 +6,20 @@ public class Math : Object
     public static float abs(float a) => global::System.Math.Abs(a);
     public static int abs(int a) => global::System.Math.Abs(a);
     public static long abs(long a) => global::System.Math.Abs(a);
-
-
-    public static double sin(double a) => global::System.Math.Sin(a);
-    public static double cos(double a) => global::System.Math.Cos(a);
-    public static double tan(double a) => global::System.Math.Tan(a);
-    public static double toRadians(double a) => a * 180d / global::System.Math.PI;
-    public static double toDegrees(double a) => a * global::System.Math.PI / 180d;
-    public static double sqrt(double a) => global::System.Math.Sqrt(a);
-    public static int min(int a, int b) => global::System.Math.Min(a, b);
-
-    public static int max(int a, int b) => global::System.Math.Max(a, b);
-
-    public static long min(long a, long b) => global::System.Math.Min(a, b);
-
-    public static long max(long a, long b) => global::System.Math.Max(a, b);
-
-
-    public static double floor(double v) => global::System.Math.Floor(v);
-
     public static double ceil(double v) => global::System.Math.Ceiling(v);
+    public static double cos(double a) => global::System.Math.Cos(a);
+    public static double floor(double v) => global::System.Math.Floor(v);
+    public static double max(double a, double b) => global::System.Math.Max(a, b);
+    public static float max(float a, float b) => global::System.Math.Max(a, b);
+    public static int max(int a, int b) => global::System.Math.Max(a, b);
+    public static long max(long a, long b) => global::System.Math.Max(a, b);
+    public static double min(double a, double b) => global::System.Math.Min(a, b);
+    public static float min(float a, float b) => global::System.Math.Min(a, b);
+    public static int min(int a, int b) => global::System.Math.Min(a, b);
+    public static long min(long a, long b) => global::System.Math.Min(a, b);
+    public static double sin(double a) => global::System.Math.Sin(a);
+    public static double sqrt(double a) => global::System.Math.Sqrt(a);
+    public static double tan(double a) => global::System.Math.Tan(a);
+    public static double toDegrees(double a) => a * global::System.Math.PI / 180d;
+    public static double toRadians(double a) => a * 180d / global::System.Math.PI;
 }
