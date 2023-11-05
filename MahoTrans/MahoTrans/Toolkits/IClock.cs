a1f9f13a7abbff484d5b0281ffcab2d9e987da42
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-11-05 03:11:49 +0300

    Add frontend-side clock APIs

--- MahoTrans/MahoTrans/Toolkits/IClock.cs
+++ MahoTrans/MahoTrans/Toolkits/IClock.cs
@@ -5,6 +5,8 @@ namespace MahoTrans.Toolkits;
 
 public interface IClock : IToolkit
 {
+    #region JVM-side APIs
+
     /// <summary>
     /// Gets current time of the system. This is used for <see cref="System.currentTimeMillis"/>.
     /// </summary>
@@ -20,13 +22,29 @@ public interface IClock : IToolkit
     long GetCurrentJvmMs(long currentTick);
 
     /// <summary>
-    /// Used to notify clock that jvm is going to sleep/wakeup
-    /// </summary>
-    bool JvmSleeping { set; }
-
-    /// <summary>
     /// Gets CLR time in which ticks bunch must be done. Ticks count is set by <see cref="JvmState.CYCLES_PER_BUNCH"/>
     /// </summary>
     /// <returns>CLR ticks.</returns>
     long GetTicksPerCycleBunch();
+
+    /// <summary>
+    /// Used to notify clock that jvm is going to sleep/wakeup
+    /// </summary>
+    bool JvmSleeping { set; }
+
+    #endregion
+
+    #region Frontend-side APIs
+
+    /// <summary>
+    /// Gets current time of the system. Returned time is in CLR format.
+    /// </summary>
+    long CurrentTime { get; }
+
+    /// <summary>
+    /// Gets current time of the system since JVM start. Returned time is in CLR format.
+    /// </summary>
+    long PassedTime { get; }
+
+    #endregion
 }
