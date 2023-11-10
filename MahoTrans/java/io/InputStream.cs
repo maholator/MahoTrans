9110ecd487d800808fd8af34461c75651efbd83d
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-11-11 01:33:22 +0300

    Add stubs in InputStream as CLDC docs state

--- MahoTrans/java/io/InputStream.cs
+++ MahoTrans/java/io/InputStream.cs
@@ -4,5 +4,21 @@ namespace java.io;
 
 public class InputStream : Object
 {
-    
+    // methods below are stubs per CLDC docs: https://nikita36078.github.io/J2ME_Docs/docs/midp-2.0/java/io/InputStream.html#close() and so on
+    public int available() => 0;
+
+    public void close()
+    {
+    }
+
+    public bool markSupported() => false;
+
+    public void mark(int readlimit)
+    {
+    }
+
+    public void reset()
+    {
+        Jvm.Throw<IOException>();
+    }
 }
