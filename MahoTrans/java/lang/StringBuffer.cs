ccd12cbedf00c5226b2d1b8195819a7e4a0b42ed
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-11-14 21:01:35 +0300

    Implement `StringBuffer` fully

--- MahoTrans/java/lang/StringBuffer.cs
+++ MahoTrans/java/lang/StringBuffer.cs
@@ -1,4 +1,5 @@
 using MahoTrans;
+using MahoTrans.Builder;
 using MahoTrans.Native;
 using MahoTrans.Runtime;
 using MahoTrans.Runtime.Types;
@@ -12,7 +13,7 @@ public class StringBuffer : Object
     [JavaIgnore] [JsonProperty] private List<char> _buffer = null!;
 
     [InitMethod]
-    public void Init() => _buffer = new List<char>();
+    public new void Init() => _buffer = new List<char>();
 
     [InitMethod]
     public void Init(int cap) => _buffer = new List<char>(cap);
@@ -38,9 +39,18 @@ public class StringBuffer : Object
     }
 
     [return: JavaType(typeof(StringBuffer))]
-    public Reference append([String] Reference s)
+    public Reference append___chars([JavaType("[C")] Reference arr)
     {
-        _buffer.AddRange(Jvm.ResolveString(s));
+        var s = Jvm.ResolveArray<char>(arr);
+        _buffer.AddRange(s);
+        return This;
+    }
+
+    [return: JavaType(typeof(StringBuffer))]
+    public Reference append___chars([JavaType("[C")] Reference arr, int offset, int len)
+    {
+        var s = Jvm.ResolveArray<char>(arr);
+        _buffer.AddRange(s.Skip(offset).Take(len));
         return This;
     }
 
@@ -94,6 +104,17 @@ public class StringBuffer : Object
     }
 
     [return: JavaType(typeof(StringBuffer))]
+    public Reference append([String] Reference s)
+    {
+        _buffer.AddRange(Jvm.ResolveString(s));
+        return This;
+    }
+
+    public int capacity() => _buffer.Capacity;
+
+    public char charAt(int index) => _buffer[index];
+
+    [return: JavaType(typeof(StringBuffer))]
     public Reference delete(int start, int end)
     {
         if (start == end)
@@ -109,8 +130,6 @@ public class StringBuffer : Object
         return This;
     }
 
-    public char charAt(int index) => _buffer[index];
-
     [return: JavaType(typeof(StringBuffer))]
     public Reference deleteCharAt(int index)
     {
@@ -120,8 +139,121 @@ public class StringBuffer : Object
         return This;
     }
 
+    public void ensureCapacity(int minCapacity)
+    {
+        if (_buffer.Capacity < minCapacity)
+            _buffer.Capacity = minCapacity;
+    }
+
+    public void getChars(int srcBegin, int srcEnd, [JavaType("[C")] Reference dstRef, int dstBegin)
+    {
+        var dst = Jvm.ResolveArray<char>(dstRef);
+
+        if (srcBegin < 0 || srcBegin > _buffer.Count || srcEnd < 0 || srcEnd > _buffer.Count)
+            Jvm.Throw<IndexOutOfBoundsException>();
+
+        _buffer.CopyTo(srcBegin, dst, dstBegin, srcEnd - srcBegin);
+    }
+
+    [return: JavaType(typeof(StringBuffer))]
+    public Reference insert(int offset, bool z)
+    {
+        _buffer.InsertRange(offset, z ? "true" : "false");
+        return This;
+    }
+
+    [return: JavaType(typeof(StringBuffer))]
+    public Reference insert(int offset, char c)
+    {
+        _buffer.Insert(offset, c);
+        return This;
+    }
+
+    [return: JavaType(typeof(StringBuffer))]
+    public Reference insert___chars(int offset, [JavaType("[C")] Reference arr)
+    {
+        var s = Jvm.ResolveArray<char>(arr);
+        _buffer.InsertRange(offset, s);
+        return This;
+    }
+
+    [return: JavaType(typeof(StringBuffer))]
+    public Reference insert(int offset, double d)
+    {
+        _buffer.InsertRange(offset, d.ToString());
+        return This;
+    }
+
+    [return: JavaType(typeof(StringBuffer))]
+    public Reference insert(int offset, float f)
+    {
+        _buffer.InsertRange(offset, f.ToString());
+        return This;
+    }
+
+    [return: JavaType(typeof(StringBuffer))]
+    public Reference insert(int offset, int i)
+    {
+        _buffer.InsertRange(offset, i.ToString());
+        return This;
+    }
+
+    [return: JavaType(typeof(StringBuffer))]
+    public Reference insert(int offset, long l)
+    {
+        _buffer.InsertRange(offset, l.ToString());
+        return This;
+    }
+
+    [JavaDescriptor("(ILjava/lang/Object;)Ljava/lang/StringBuffer;")]
+    public JavaMethodBody insert(JavaClass cls)
+    {
+        var b = new JavaMethodBuilder(cls);
+
+        b.AppendThis();
+        b.Append(JavaOpcode.iload_1);
+        b.Append(JavaOpcode.aload_2);
+        b.AppendVirtcall("toString", typeof(String));
+        b.AppendVirtcall(nameof(StringBuffer.insert), "(ILjava/lang/String;)Ljava/lang/StringBuffer;");
+        b.AppendThis();
+        b.AppendReturnReference();
+
+        return b.Build(3, 3);
+    }
+
+    [return: JavaType(typeof(StringBuffer))]
+    public Reference insert(int offset, [String] Reference s)
+    {
+        _buffer.InsertRange(offset, Jvm.ResolveString(s));
+        return This;
+    }
 
     public int length() => _buffer.Count;
+
+    [return: JavaType(typeof(StringBuffer))]
+    public Reference reverse()
+    {
+        _buffer.Reverse();
+        return This;
+    }
+
+    public void setCharAt(int index, char c)
+    {
+        _buffer[index] = c;
+    }
+
+    public void setLength(int newLength)
+    {
+        if (_buffer.Count == newLength)
+            return;
+        if (_buffer.Count < newLength)
+        {
+            _buffer.AddRange(Enumerable.Repeat('\0', newLength - _buffer.Count));
+            return;
+        }
+
+        _buffer.RemoveRange(newLength, _buffer.Count - newLength);
+    }
 
     [return: String]
     public Reference toString()
