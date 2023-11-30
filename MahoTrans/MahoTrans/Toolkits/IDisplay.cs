8e5d4cc1ec388691c6923be43d15fc220dfe0c71
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-11-30 17:48:41 +0300

    Add toolkit method to invalidate lcdui and docs

--- MahoTrans/MahoTrans/Toolkits/IDisplay.cs
+++ MahoTrans/MahoTrans/Toolkits/IDisplay.cs
@@ -3,13 +3,33 @@ using MahoTrans.Runtime;
 
 namespace MahoTrans.Toolkits;
 
+/// <summary>
+/// Toolkit which is responsible for displaying midlet's UI in emulator's UI.
+/// </summary>
 public interface IDisplay : IToolkit
 {
+    /// <summary>
+    /// Notifies toolkit about newly constructed displayable.
+    /// </summary>
+    /// <param name="d">Object of created displayable. Toolkit must not store it in any way.</param>
+    /// <returns>Handle for communicating with the toolkit.</returns>
     DisplayableHandle Register(Displayable d);
 
+    /// <summary>
+    /// Sets the displayable as current on display.
+    /// </summary>
+    /// <param name="handle">Displayable's handle.</param>
     void SetCurrent(DisplayableHandle handle);
+
+    /// <summary>
+    /// Removes a displayable from display.
+    /// </summary>
     void SetNullCurrent();
 
+    /// <summary>
+    /// Gets current displayable on display.
+    /// </summary>
+    /// <returns>Displayable's handle.</returns>
     DisplayableHandle? GetCurrent();
 
     int GetWidth(DisplayableHandle handle);
@@ -20,9 +40,34 @@ public interface IDisplay : IToolkit
 
     void SetFullscreen(DisplayableHandle handle, bool state);
 
+    /// <summary>
+    /// Notifies toolkit about displayable's title change.
+    /// </summary>
+    /// <param name="handle">Displayable's handle.</param>
+    /// <param name="title">New title as CLR string.</param>
     void SetTitle(DisplayableHandle handle, string title);
 
-    void CommandsRefreshed(DisplayableHandle handle, List<Reference> commands);
+    /// <summary>
+    /// Notifies toolkit that commands list on the displayable was changed.
+    /// </summary>
+    /// <param name="handle">Displayable's handle.</param>
+    /// <param name="commands">New list of commands. List is unsorted.
+    /// All objects are guaranteed to be <see cref="Command"/>s.
+    /// </param>
+    void CommandsUpdated(DisplayableHandle handle, List<Reference> commands);
+
+    /// <summary>
+    /// Notifies toolkit that screen displayable changed its content (text, subitems, etc.).
+    /// </summary>
+    /// <param name="handle">Displayable's handle.</param>
+    /// <remarks>
+    /// Title changes are reported via <see cref="SetTitle"/>.<br/>
+    /// Fullscreen mode is set via <see cref="SetFullscreen"/>.<br/>
+    /// Commands changes are reported via <see cref="CommandsUpdated"/>.<br/>
+    /// Framebuffer changes are reported via <see cref="Flush(MahoTrans.Toolkits.DisplayableHandle)"/>.<br/>
+    /// This is used only for LCDUI things (alert text/image, list items, form items, textbox content).
+    /// </remarks>
+    void ContentUpdated(DisplayableHandle handle);
 
     GraphicsHandle GetGraphics(DisplayableHandle handle);
 
@@ -30,5 +75,9 @@ public interface IDisplay : IToolkit
 
     void Flush(DisplayableHandle handle, int x, int y, int width, int height);
 
+    /// <summary>
+    /// Notifies toolkit that this displayable is dead.
+    /// </summary>
+    /// <param name="handle">Displayable's handle. It must not be used anymore.</param>
     void Release(DisplayableHandle handle);
 }
