e54745291d0d51d8f8aa4832e6c604aeaa8a94ac
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-11-30 12:15:44 +0300

    Partially implement `Alert`

--- MahoTrans/javax/microedition/lcdui/Alert.cs
+++ MahoTrans/javax/microedition/lcdui/Alert.cs
@@ -1,5 +1,71 @@
+// ReSharper disable InconsistentNaming
+
+using java.lang;
+using MahoTrans.Native;
+using MahoTrans.Runtime;
+
 namespace javax.microedition.lcdui;
 
 public class Alert : Screen
 {
+    public static int FOREVER = -2;
+
+    [InitMethod]
+    public void Init([String] Reference title)
+    {
+        Init(title, Reference.Null, Reference.Null, Reference.Null);
+    }
+
+    [InitMethod]
+    public void Init([String] Reference title, [String] Reference alertText,
+        [JavaType(typeof(Image))] Reference alertImage, [JavaType(typeof(AlertType))] Reference alertType)
+    {
+        setTitle(title);
+        Text = alertText;
+        Image = alertImage;
+        Type = alertType;
+        Timeout = getDefaultTimeout();
+    }
+
+    [String] public Reference Text;
+
+    [JavaType(typeof(Image))] public Reference Image;
+
+    [JavaType(typeof(AlertType))] public Reference Type;
+
+    public int Timeout;
+
+    public int getDefaultTimeout() => FOREVER;
+
+    public int getTimeout() => Timeout;
+
+    public void setTimeout(int timeout)
+    {
+        if (timeout <= 0 && timeout != FOREVER)
+            Jvm.Throw<IllegalArgumentException>();
+
+        Timeout = timeout;
+    }
+
+    [return: JavaType(typeof(AlertType))]
+    public Reference getType() => Type;
+
+    public void setType([JavaType(typeof(AlertType))] Reference type)
+    {
+        Type = type;
+    }
+
+    [return: String]
+    public Reference getString() => Text;
+
+    public void setString([String] Reference text) => Text = text;
+
+    [return: JavaType(typeof(Image))]
+    public Reference getImage() => Image;
+
+    public void setImage([JavaType(typeof(Image))] Reference image) => Image = image;
+
+    //TODO set/get Indicator
+
+    //TODO commands
 }
