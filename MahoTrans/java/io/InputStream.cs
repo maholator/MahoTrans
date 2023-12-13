b687f285255e0738f3c498db6dc970bdcb9bffca
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-12-14 01:38:49 +0300

    Implement `InputStream.skip(J)J`

--- MahoTrans/java/io/InputStream.cs
+++ MahoTrans/java/io/InputStream.cs
@@ -41,6 +41,57 @@ public class InputStream : Object
     {
     }
 
+    [JavaDescriptor("(J)J")]
+    public JavaMethodBody skip(JavaClass cls)
+    {
+        // locals: this, count, i, read
+        var b = new JavaMethodBuilder(cls);
+
+        // i=0; read=0;
+        b.Append(JavaOpcode.lconst_0);
+        b.Append(JavaOpcode.lstore_2);
+        b.Append(JavaOpcode.lconst_0);
+        b.Append(JavaOpcode.lstore_3);
+
+        using (var loop = b.BeginLoop(JavaOpcode.iflt))
+        {
+            b.AppendThis();
+            b.AppendVirtcall(nameof(read), typeof(int));
+            using (b.AppendGoto(JavaOpcode.ifge))
+            {
+                // read = i; i = count;
+                b.Append(JavaOpcode.lload_2);
+                b.Append(JavaOpcode.lstore_3);
+                b.Append(JavaOpcode.lload_1);
+                b.Append(JavaOpcode.lstore_2);
+            }
+
+            // i = i + 1L;
+            b.Append(JavaOpcode.lload_2);
+            b.Append(JavaOpcode.lconst_1);
+            b.Append(JavaOpcode.ladd);
+            b.Append(JavaOpcode.lstore_2);
+
+            loop.ConditionSection();
+
+            b.Append(JavaOpcode.lload_2);
+            b.Append(JavaOpcode.lload_1);
+            b.Append(JavaOpcode.lcmp);
+        }
+
+        b.Append(JavaOpcode.lload_3);
+        using (b.AppendGoto(JavaOpcode.ifeq))
+        {
+            b.Append(JavaOpcode.lload_3);
+            b.AppendReturnLong();
+        }
+
+        b.Append(JavaOpcode.lload_1);
+        b.AppendReturnLong();
+
+        return b.Build(2, 4);
+    }
+
     public void reset()
     {
         Jvm.Throw<IOException>();
