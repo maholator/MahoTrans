2ca4a638150eb302a6ed14163184e2cc8af1324c
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-12-14 03:23:33 +0300

    Throw MediaException on mmapi usage attempts

--- MahoTrans/javax/microedition/media/Manager.cs
+++ MahoTrans/javax/microedition/media/Manager.cs
@@ -9,6 +9,14 @@ public class Manager : Object
     [JavaDescriptor("(Ljava/io/InputStream;Ljava/lang/String;)Ljavax/microedition/media/Player;")]
     public static Reference createPlayer(Reference stream, Reference str)
     {
+        Jvm.Throw<MediaException>();
+        return Reference.Null;
+    }
+
+    [return: JavaType(typeof(Player))]
+    public static Reference createPlayer([String] Reference mrl)
+    {
+        Jvm.Throw<MediaException>();
         return Reference.Null;
     }
 }
