7c6fc7297bf1fe1bfdb739747b6aa553c5cbb557
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2023-12-23 16:20:20 +0300

    Stub `DeviceControl`

---
+++ MahoTrans/com/nokia/mid/ui/DeviceControl.cs
@@ -0,0 +1,12 @@
+using Object = java.lang.Object;
+
+namespace com.nokia.mid.ui;
+
+public class DeviceControl : Object
+{
+    public static void setLights(int num,
+        int level)
+    {
+        //TODO implement
+    }
+}
