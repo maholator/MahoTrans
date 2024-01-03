7c0a7a8476981400dcc91e360b6b2e3834f89cf0
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-01-04 02:54:51 +0300

    Implement missing `Character` methods

--- MahoTrans/java/lang/Character.cs
+++ MahoTrans/java/lang/Character.cs
@@ -2,12 +2,18 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using MahoTrans.Native;
+using MahoTrans.Runtime;
 
 namespace java.lang;
 
 public class Character : Object
 {
     public char Value;
+
+    public const int MAX_RADIX = 36;
+    public const char MAX_VALUE = (char)65535;
+    public const int MIN_RADIX = 2;
+    public const char MIN_VALUE = (char)0;
 
     [InitMethod]
     public void Init(char c) => Value = c;
@@ -34,5 +40,30 @@ public class Character : Object
         if (val < radix)
             return val;
         return -1;
+    }
+
+    public static char toLowerCase(char ch) => char.ToLower(ch);
+
+    public static char toUpperCase(char ch) => char.ToUpper(ch);
+
+    public static bool isLowerCase(char ch) => char.IsLower(ch);
+
+    public static bool isUpperCase(char ch) => char.IsUpper(ch);
+
+    public static bool isDigit(char ch) => char.IsDigit(ch);
+
+    [return: String]
+    public Reference toString() => Jvm.AllocateString($"{Value}");
+
+    public new int hashCode() => Value;
+
+    public new bool equals(Reference obj)
+    {
+        if (obj.IsNull)
+            return false;
+        var o = Jvm.ResolveObject(obj);
+        if (o is not Character c)
+            return false;
+        return c.Value == Value;
     }
 }
