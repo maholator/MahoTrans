9b6b0eea923d0ec7d671df4febd001568bef4565
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-01-06 16:39:38 +0300

    Implement `DataInputStream::readFully([BII)V`

--- MahoTrans/java/io/DataInputStream.cs
+++ MahoTrans/java/io/DataInputStream.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Fyodor Ryzhov. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using java.lang;
 using MahoTrans;
 using MahoTrans.Builder;
 using MahoTrans.Native;
@@ -98,6 +99,66 @@ public class DataInputStream : InputStream
                 new(JavaOpcode.@return),
             }
         };
+    }
+
+    [JavaDescriptor("([BII)V")]
+    public JavaMethodBody readFully___bounds(JavaClass cls)
+    {
+        var b = new JavaMethodBuilder(cls);
+
+        // If off is negative...
+        b.Append(JavaOpcode.iload_2);
+        using (b.AppendGoto(JavaOpcode.ifge))
+        {
+            b.AppendNewObject<IndexOutOfBoundsException>();
+            b.Append(JavaOpcode.athrow);
+        }
+
+        // ...or len is negative...
+        b.Append(JavaOpcode.iload_3);
+        using (b.AppendGoto(JavaOpcode.ifge))
+        {
+            b.AppendNewObject<IndexOutOfBoundsException>();
+            b.Append(JavaOpcode.athrow);
+        }
+
+        // ...or off+len is greater than the length of the array b...
+        b.Append(JavaOpcode.iload_2);
+        b.Append(JavaOpcode.iload_3);
+        b.Append(JavaOpcode.iadd);
+        b.Append(JavaOpcode.aload_1);
+        b.Append(JavaOpcode.arraylength);
+        using (b.AppendGoto(JavaOpcode.if_icmple))
+        {
+            b.AppendNewObject<IndexOutOfBoundsException>();
+            b.Append(JavaOpcode.athrow);
+        }
+        // ...then an IndexOutOfBoundsException is thrown.
+
+        b.Append(JavaOpcode.iload_2);
+        b.Append(JavaOpcode.istore, 4);
+
+        using (var loop = b.BeginLoop(JavaOpcode.if_icmplt))
+        {
+            b.Append(JavaOpcode.aload_1);
+            b.Append(JavaOpcode.iload, 4);
+            b.AppendThis();
+            b.AppendVirtcall(nameof(readByte), typeof(sbyte));
+            b.Append(JavaOpcode.bastore);
+
+            b.AppendInc(4, 1);
+
+            loop.ConditionSection();
+
+            b.Append(JavaOpcode.iload, 4);
+            b.Append(JavaOpcode.iload_2);
+            b.Append(JavaOpcode.iload_3);
+            b.Append(JavaOpcode.iadd);
+        }
+
+        b.AppendReturn();
+
+        return b.Build(3, 5);
     }
 
     [JavaDescriptor("()S")]
