d981907d9e467de5b88106350521259dd37add12
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-01-15 00:27:37 +0300

    Stub nokia sound class

---
+++ MahoTrans/com/nokia/mid/sound/Sound.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Fyodor Ryzhov. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using Object = java.lang.Object;
+
+namespace com.nokia.mid.sound;
+
+public class Sound : Object
+{
+}
