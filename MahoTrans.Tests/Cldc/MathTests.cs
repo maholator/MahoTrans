33ad5b278bb328a3790e1f6d3b0e968cfb0867af
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-01-16 02:19:26 +0300

    Add basic math & wide iinc tests

---
+++ MahoTrans.Tests/Cldc/MathTests.cs
@@ -0,0 +1,18 @@
+// Copyright (c) Fyodor Ryzhov. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace MahoTrans.Tests.Cldc;
+
+public class MathTests : JarTestBase
+{
+    [SetUp]
+    public void Setup() => DetachHeap();
+
+    [Test]
+    public void TestBasic() => Load("TestBasic").Run();
+
+    [Test]
+    public void WideIinc() => Load("WideIinc").Run();
+
+    public override string ClassName => "MathTests";
+}
