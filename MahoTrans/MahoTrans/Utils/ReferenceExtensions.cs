2b4b02295f7e18a588eed56f1e40aa129b1cba59
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-01-16 16:45:35 +0300

    Add docs for ref extensions

--- MahoTrans/MahoTrans/Utils/ReferenceExtensions.cs
+++ MahoTrans/MahoTrans/Utils/ReferenceExtensions.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Fyodor Ryzhov. Licensed under the MIT Licence.
+// Copyright (c) Fyodor Ryzhov / Arman Jussupgaliyev. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
 using MahoTrans.Runtime;
@@ -6,18 +6,30 @@ using Object = java.lang.Object;
 
 namespace MahoTrans.Utils;
 
+/// <summary>
+///     Compact ways to call resolution methods.
+/// </summary>
 public static class ReferenceExtensions
 {
+    /// <summary>
+    ///     Calls <see cref="JvmState.ResolveObject" />.
+    /// </summary>
     public static Object AsObject(this Reference r)
     {
         return JvmState.Context.ResolveObject(r);
     }
 
+    /// <summary>
+    ///     Calls <see cref="JvmState.Resolve{T}" />.
+    /// </summary>
     public static T As<T>(this Reference r) where T : Object
     {
         return JvmState.Context.Resolve<T>(r);
     }
 
+    /// <summary>
+    ///     Calls <see cref="JvmState.ResolveNullable{T}" />.
+    /// </summary>
     public static T? AsNullable<T>(this Reference r) where T : Object
     {
         return JvmState.Context.ResolveNullable<T>(r);
