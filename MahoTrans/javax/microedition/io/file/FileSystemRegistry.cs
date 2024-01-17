e53b7950f627a51b1d6d43303b1bde6899962438
Shinovon <arman200711@gmail.com>
2024-01-17 17:11:20 +0500

    Stub `FileSystemRegistry.listRoots()`

--- MahoTrans/javax/microedition/io/file/FileSystemRegistry.cs
+++ MahoTrans/javax/microedition/io/file/FileSystemRegistry.cs
@@ -1,10 +1,20 @@
 ﻿// Copyright (c) Arman Jussupgaliyev. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using java.util;
+using MahoTrans.Native;
+using MahoTrans.Runtime;
 using Object = java.lang.Object;
+using IOException = java.io.IOException;
 
 namespace javax.microedition.io.file;
 
 public class FileSystemRegistry : Object
 {
+    [JavaDescriptor("()Ljava/util/Enumeration;")]
+    public static Reference listRoots()
+    {
+        Jvm.Throw<IOException>();
+        return default;
+    }
 }
