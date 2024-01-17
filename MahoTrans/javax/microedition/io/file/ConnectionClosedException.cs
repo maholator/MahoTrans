6acbec07326abf619ff2807d0410e1a064e6b9e4
Shinovon <arman200711@gmail.com>
2024-01-17 07:35:41 +0500

    jsr 75 exceptions

---
+++ MahoTrans/javax/microedition/io/file/ConnectionClosedException.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Arman Jussupgaliyev. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using MahoTrans.Native;
+using MahoTrans.Runtime;
+using IOException = java.io.IOException;
+
+namespace javax.microedition.io.file;
+
+public class ConnectionClosedException : IOException
+{
+    [InitMethod]
+    public new void Init()
+    {
+        base.Init();
+    }
+
+    [InitMethod]
+    public new void Init([String] Reference message)
+    {
+        base.Init(message);
+    }
+}
