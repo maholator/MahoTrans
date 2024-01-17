cdd8b4fe7da2ba8c0d5cf65dec3a771dc0bb34a8
Shinovon <arman200711@gmail.com>
2024-01-17 07:38:01 +0500

    Stub `FileSystemRegistry`

---
+++ MahoTrans/javax/microedition/io/file/FileSystemListener.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Fyodor Ryzhov. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using MahoTrans.Native;
+
+namespace javax.microedition.io.file;
+
+[JavaInterface]
+public interface FileSystemListener
+{
+}
---
+++ MahoTrans/javax/microedition/io/file/FileSystemRegistry.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Fyodor Ryzhov. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using Object = java.lang.Object;
+
+namespace javax.microedition.io.file;
+
+public class FileSystemRegistry : Object
+{
+}
