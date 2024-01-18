41bc07b1f99b7154acf68f86622cff3ade096247
Shinovon <arman200711@gmail.com>
2024-01-18 14:43:06 +0500

    `MIDletStateChangeException`

---
+++ MahoTrans/javax/microedition/midlet/MIDletStateChangeException.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Arman Jussupgaliyev. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using MahoTrans.Native;
+using MahoTrans.Runtime;
+using Exception = java.lang.Exception;
+
+namespace javax.microedition.midlet;
+
+public class MIDletStateChangeException : Exception
+{
+    [InitMethod]
+    public new void Init()
+    {
+        base.Init();
+    }
+
+    [InitMethod]
+    public new void Init([String] Reference message)
+    {
+        base.Init(message);
+    }
+}
