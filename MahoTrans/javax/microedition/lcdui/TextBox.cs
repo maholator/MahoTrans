9a7ca81aaf1d6f34770c116911e39f3020fb41d1
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-01-28 16:57:13 +0300

    Implement text set on textbox

--- MahoTrans/javax/microedition/lcdui/TextBox.cs
+++ MahoTrans/javax/microedition/lcdui/TextBox.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Fyodor Ryzhov. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using java.lang;
 using MahoTrans.Native;
 using MahoTrans.Runtime;
 
@@ -15,6 +16,8 @@ public class TextBox : Screen
     [InitMethod]
     public void Init([String] Reference title, [String] Reference text, int maxSize, int constraints)
     {
+        if (maxSize <= 0)
+            Jvm.Throw<IllegalArgumentException>();
         base.Init();
         setTitle(title);
         Content = Jvm.ResolveStringOrDefault(text) ?? "";
@@ -25,6 +28,15 @@ public class TextBox : Screen
 
     [return: String]
     public Reference getString() => Jvm.InternalizeString(Content);
+
+    public void setString([String] Reference text)
+    {
+        var t = Jvm.ResolveStringOrDefault(text) ?? "";
+        if (t.Length > MaxSize)
+            Jvm.Throw<IllegalArgumentException>();
+        Content = t;
+        Toolkit.Display.ContentUpdated(Handle);
+    }
 
     public int getMaxSize() => MaxSize;
 }
