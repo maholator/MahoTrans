c5127b9fab0a4ef4df29c1f1ad64a67c599836f0
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-02-08 18:11:51 +0300

    More references

--- MahoTrans/com/nokia/mid/ui/DirectGraphics.cs
+++ MahoTrans/com/nokia/mid/ui/DirectGraphics.cs
@@ -1,11 +1,12 @@
 // Copyright (c) Fyodor Ryzhov. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using javax.microedition.lcdui;
 using MahoTrans.Native;
 
 namespace com.nokia.mid.ui;
 
-[JavaInterface]
+[JavaInterface(typeof(Graphics))]
 public interface DirectGraphics
 {
 }
