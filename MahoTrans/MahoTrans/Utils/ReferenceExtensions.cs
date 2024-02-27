8106f97809279acc5e798c9edaec9874e91a63cb
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-02-27 21:44:18 +0300

    Force inlining of extensions

--- MahoTrans/MahoTrans/Utils/ReferenceExtensions.cs
+++ MahoTrans/MahoTrans/Utils/ReferenceExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Fyodor Ryzhov / Arman Jussupgaliyev. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Runtime.CompilerServices;
 using MahoTrans.Runtime;
 using Object = java.lang.Object;
 
@@ -14,6 +15,7 @@ public static class ReferenceExtensions
     /// <summary>
     ///     Calls <see cref="JvmState.ResolveObject" />.
     /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static Object AsObject(this Reference r)
     {
         return JvmContext.Jvm!.ResolveObject(r);
@@ -22,6 +24,7 @@ public static class ReferenceExtensions
     /// <summary>
     ///     Calls <see cref="JvmState.Resolve{T}" />.
     /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static T As<T>(this Reference r) where T : Object
     {
         return JvmContext.Jvm!.Resolve<T>(r);
@@ -30,16 +33,21 @@ public static class ReferenceExtensions
     /// <summary>
     ///     Calls <see cref="JvmState.ResolveOrNull{T}" />.
     /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static T? AsOrNull<T>(this Reference r) where T : Object
     {
         return JvmContext.Jvm!.ResolveOrNull<T>(r);
     }
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static string AsString(this Reference r) => JvmContext.Jvm!.ResolveString(r);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static string? AsStringOrNull(this Reference r) => JvmContext.Jvm!.ResolveStringOrNull(r);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static T[] AsArray<T>(this Reference r) where T : struct => JvmContext.Jvm!.ResolveArray<T>(r);
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
     public static T[]? AsArrayOrNull<T>(this Reference r) where T : struct => JvmContext.Jvm!.ResolveArrayOrNull<T>(r);
 }
