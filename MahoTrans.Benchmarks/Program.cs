f721d04d3f192eb3e7bf955fa7cb6a37d26db85b
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-02-27 19:04:40 +0300

    Add benchmarks project again

---
+++ MahoTrans.Benchmarks/Program.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+namespace MahoTrans.Benchmarks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = DefaultConfig.Instance
+            .AddJob(Job.Default.WithId(".NET 6").WithRuntime(CoreRuntime.Core60))
+            .AddJob(Job.Default.WithId(".NET 8 w/o PGO").WithRuntime(CoreRuntime.Core80).WithEnvironmentVariable("DOTNET_TieredPGO", "0"))
+            .AddJob(Job.Default.WithId(".NET 8").WithRuntime(CoreRuntime.Core80));
+        BenchmarkSwitcher
+            .FromAssembly(typeof(Program).Assembly)
+            .Run(args, config);
+    }
+}
