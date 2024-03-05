781a97345c7ef5081bd68b3509d7cfe742375f69
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-03-06 02:42:39 +0300

    Split constants pass to own part

--- MahoTrans/MahoTrans/Compiler/CrossRoutineCompilerPass.cs
+++ MahoTrans/MahoTrans/Compiler/CrossRoutineCompilerPass.cs
@@ -154,6 +154,7 @@ public partial class CrossRoutineCompilerPass
             }
         }
 
+        // all returned values were returned.
         _il.Emit(OpCodes.Ret);
     }
 
@@ -189,77 +190,5 @@ public partial class CrossRoutineCompilerPass
         }
 
         throw new JavaLinkageException("Failed to find value consumer");
-    }
-
-    public void PushConstant(LinkedInstruction instr)
-    {
-        using (new MarshallerWrapper(this, ^1))
-        {
-            switch (instr.Opcode)
-            {
-                case MTOpcode.iconst_m1:
-                    _il.Emit(OpCodes.Ldc_I4_M1);
-                    break;
-                case MTOpcode.iconst_0:
-                    _il.Emit(OpCodes.Ldc_I4_0);
-                    break;
-                case MTOpcode.iconst_1:
-                    _il.Emit(OpCodes.Ldc_I4_1);
-                    break;
-                case MTOpcode.iconst_2:
-                    _il.Emit(OpCodes.Ldc_I4_2);
-                    break;
-                case MTOpcode.iconst_3:
-                    _il.Emit(OpCodes.Ldc_I4_3);
-                    break;
-                case MTOpcode.iconst_4:
-                    _il.Emit(OpCodes.Ldc_I4_4);
-                    break;
-                case MTOpcode.iconst_5:
-                    _il.Emit(OpCodes.Ldc_I4_5);
-                    break;
-                case MTOpcode.lconst_0:
-                    _il.Emit(OpCodes.Ldc_I8, 0L);
-                    break;
-                case MTOpcode.lconst_1:
-                    _il.Emit(OpCodes.Ldc_I8, 1L);
-                    break;
-                case MTOpcode.lconst_2:
-                    _il.Emit(OpCodes.Ldc_I8, 2L);
-                    break;
-                case MTOpcode.fconst_0:
-                    _il.Emit(OpCodes.Ldc_R4, 0F);
-                    break;
-                case MTOpcode.fconst_1:
-                    _il.Emit(OpCodes.Ldc_R4, 1F);
-                    break;
-                case MTOpcode.fconst_2:
-                    _il.Emit(OpCodes.Ldc_R4, 2F);
-                    break;
-                case MTOpcode.dconst_0:
-                    _il.Emit(OpCodes.Ldc_R8, 0D);
-                    break;
-                case MTOpcode.dconst_1:
-                    _il.Emit(OpCodes.Ldc_R8, 1D);
-                    break;
-                case MTOpcode.dconst_2:
-                    _il.Emit(OpCodes.Ldc_R8, 2D);
-                    break;
-                case MTOpcode.iconst:
-                    _il.Emit(OpCodes.Ldc_I4, instr.IntData);
-                    break;
-                case MTOpcode.strconst:
-                    _il.Emit(OpCodes.Ldstr, (string)instr.Data);
-                    break;
-                case MTOpcode.lconst:
-                    _il.Emit(OpCodes.Ldc_I8, (long)instr.Data);
-                    break;
-                case MTOpcode.dconst:
-                    _il.Emit(OpCodes.Ldc_R8, (double)instr.Data);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
     }
 }
