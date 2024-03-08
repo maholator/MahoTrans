c73690f5878bbe4f6f13a709dc9ab172ce4910f5
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-03-08 18:03:04 +0300

    Add load progress loggers

--- MahoTrans.Abstractions/Abstractions/ILoadLogger.cs
+++ MahoTrans.Abstractions/Abstractions/ILoadLogger.cs
@@ -14,5 +14,27 @@ public interface ILoadLogger : IToolkit
     /// <param name="type">Type of the issue.</param>
     /// <param name="className">Name of the class, where the issue was detected.</param>
     /// <param name="message">Description of the issue.</param>
-    public void Log(LoadIssueType type, string className, string message);
+    void Log(LoadIssueType type, string className, string message);
+
+    /// <summary>
+    ///     Reports progress of linker's work.
+    /// </summary>
+    /// <param name="num">Number of class that will be processed.</param>
+    /// <param name="total">Total number of classes.</param>
+    /// <param name="name">Name of the class.</param>
+    /// <remarks>
+    ///     This never achieves 100% (num == total) because this reports BEFORE the work starts.
+    /// </remarks>
+    void ReportLinkProgress(int num, int total, string name);
+
+    /// <summary>
+    ///     Reports progress of compiler's work.
+    /// </summary>
+    /// <param name="num">Number of class that will be processed.</param>
+    /// <param name="total">Total number of classes.</param>
+    /// <param name="name">Name of the class.</param>
+    /// <remarks>
+    ///     This never achieves 100% (num == total) because this reports BEFORE the work starts.
+    /// </remarks>
+    void ReportCompileProgress(int num, int total, string name);
 }
