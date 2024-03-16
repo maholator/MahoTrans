c7f5168ef4b5c23207f15ed41156e644324dec8b
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-03-16 14:00:43 +0300

    Stub `Player`
    #15 part

--- MahoTrans/javax/microedition/media/Player.cs
+++ MahoTrans/javax/microedition/media/Player.cs
@@ -2,10 +2,39 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using MahoTrans.Native;
+using MahoTrans.Runtime;
+using String = java.lang.String;
 
 namespace javax.microedition.media;
 
 [JavaInterface]
 public interface Player : Controllable
 {
+    public void addPlayerListener([JavaType(typeof(PlayerListener))] Reference listener) => throw new AbstractCall();
+
+    public void close() => throw new AbstractCall();
+
+    public void deallocate() => throw new AbstractCall();
+
+    public String getContentType() => throw new AbstractCall();
+
+    public long getDuration() => throw new AbstractCall();
+
+    public long getMediaTime() => throw new AbstractCall();
+
+    public int getState() => throw new AbstractCall();
+
+    public void prefetch() => throw new AbstractCall();
+
+    public void realize() => throw new AbstractCall();
+
+    public void removePlayerListener([JavaType(typeof(PlayerListener))] Reference listener) => throw new AbstractCall();
+
+    public void setLoopCount(int count) => throw new AbstractCall();
+
+    public long setMediaTime(long time) => throw new AbstractCall();
+
+    public void start() => throw new AbstractCall();
+
+    public void stop() => throw new AbstractCall();
 }
