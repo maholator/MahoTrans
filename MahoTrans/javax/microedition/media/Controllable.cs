3efbf56fa5fc2d02ab533215fc728eef450f1080
Fyodor Ryzhov <53872073+Feodor0090@users.noreply.github.com>
2024-03-17 23:45:37 +0300

    Stub `DirectGraphics` and `Controllable`
    Part of #15. These two were too loud.

--- MahoTrans/javax/microedition/media/Controllable.cs
+++ MahoTrans/javax/microedition/media/Controllable.cs
@@ -2,9 +2,15 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using MahoTrans;
+using MahoTrans.Native;
+using MahoTrans.Runtime;
 
 namespace javax.microedition.media;
 
 public interface Controllable : IJavaObject
 {
+    public Control getControl(string controlType) => throw new AbstractCall();
+
+    [return: JavaType(typeof(Control[]))]
+    public Reference getControls() => throw new AbstractCall();
 }
